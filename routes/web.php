0e3ff598388b1847b6e7d418ac83de21747453fa
AleGonzalezTrigo <147615266+AleGonzalezTrigo@users.noreply.github.com>
2026-05-10 20:34:28 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ViewController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ViewController::class, 'dashboard'])->name('dashboard');
+Route::get('/productos', [ViewController::class, 'productos'])->name('productos.index');
+Route::get('/pedidos', [ViewController::class, 'pedidos'])->name('pedidos.index');
+Route::get('/clientes', [ViewController::class, 'clientes'])->name('clientes.index');
+Route::get('/estadisticas', [ViewController::class, 'estadisticas'])->name('estadisticas');
+Route::get('/configuracion', [ViewController::class, 'configuracion'])->name('configuracion');
